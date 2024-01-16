9582bcb4cae1ea121c9d1b37c48c538497cde251
FarahNurFadhilah <140325614+FarahNurFadhilah@users.noreply.github.com>
2024-01-16 22:28:38 +0700

    Variable, Tipe data, dan Komentar

---
+++ phpsmkrevit/lat02.php
@@ -0,0 +1,20 @@
+<?php 
+
+$tulisan ='Saya sedang bermain';
+
+echo $tulisan.'<br>';
+
+$angka = '2000';
+
+echo 'Tahun'.$angka.'<br>';
+
+var_dump($tulisan);
+
+echo '<br>';
+
+var_dump((int)$angka+20);
+
+
+
+
+?>
