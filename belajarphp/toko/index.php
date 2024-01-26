9b0a5a0cfa3d309403388cb4d1ace1e06e0b0280
FarahNurFadhilah <140325614+FarahNurFadhilah@users.noreply.github.com>
2024-01-26 10:05:37 +0700

    jum'at,26 januari

--- belajarphp/toko/index.php
+++ belajarphp/toko/index.php
@@ -1,3 +1,18 @@
+<form action="" method="post">
+    barang:
+    <input type="text" name="namabarang" placeholder="namabarang">
+    <br>
+    harga:
+    <input type="number" name="harga" placeholder="harga barang">
+    <br>
+    stok:
+    <input type="number" name="stok" placeholder="stok barang">
+    <br>
+    <input type="submit" name="simpan" value="simpan">
+
+
+</form>
+
 <?php 
 
 $host="127.0.0.1";
@@ -5,6 +20,16 @@ $user="root";
 $password="";
 $db="dbtoko";
 $koneksi=new mysqli($host,$user,$password,$db);
+
+if(isset($_POST["simpan"])){
+    $namabarang=$_POST["namabarang"];
+    $harga=$_POST["harga"];
+    $stok=$_POST["stok"];
+
+$sql= "INSERT INTO barang (namabarang,harga,stok) VALUES ('$namabarang',$harga,$stok)";
+$hasil=mysqli_query($koneksi,$sql);
+}
+
 
 $sql="SELECT*FROM barang";
 
@@ -32,13 +57,20 @@ while($row=mysqli_fetch_array($hasil)){
     echo "<tr>";
     echo "<td>".$row[1]. "</td>";
     echo "<td>".$row[2].   "</td>";
-    echo "<td>" .$row[3] .  "</td>";
+    echo "<td>" .$row[3].  "</td>";
     echo "</tr>";
 }
 echo "</tbody>
 </table>";
 
 //PELANGGAN
+
+$namapelanggan="aku";
+$alamat="indonesia";
+$notelepon=99999;
+
+$sql= "INSERT INTO pelanggan (namapelanggan,alamat,notelepon) VALUES ('$namapelanggan','$alamat',$notelepon)";
+$hasil=mysqli_query($koneksi,$sql);
 
 $sql="SELECT*FROM pelanggan";
 $hasil=mysqli_query($koneksi,$sql);
