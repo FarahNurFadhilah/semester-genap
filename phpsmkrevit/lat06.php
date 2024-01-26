4ade703beb65a9eb20b6ba177c2959347cc118c0
FarahNurFadhilah <140325614+FarahNurFadhilah@users.noreply.github.com>
2024-01-27 00:13:01 +0700

    Pemilihan atau selector PHP

---
+++ phpsmkrevit/lat06.php
@@ -0,0 +1,41 @@
+<?php 
+
+$hari = 4;
+
+// switch ($hari){
+//     case 1:
+//         echo "minggu";
+//         break;
+//     case 2:
+//         echo "senin";
+//         break;
+//     case 3:
+//         echo "selasa";
+//         break;
+//     case 4:
+//         echo "rabu";
+//         break;
+//     default:
+//         echo "hari belum dibuat";
+//         break;
+// }
+
+$pilihan = "tambah";
+
+switch ($pilihan){
+    case "tambah":
+        echo "Anda memilih tambah";
+        break;
+    case "ubah":
+        echo "Anda memilih ubah";
+        break;
+    case "hapus":
+        echo "Anda memilih hapus";
+        break;
+    default:
+        echo "Pilihan belum ada";
+        break;
+}
+
+
+?>
