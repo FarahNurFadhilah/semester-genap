95e9ba2f1751642fbc667de5f223e25b5f6822d3
FarahNurFadhilah <140325614+FarahNurFadhilah@users.noreply.github.com>
2024-02-05 13:33:40 +0700

    Post

--- phpsmkrevit/nav.php
+++ phpsmkrevit/nav.php
@@ -4,4 +4,21 @@
         <li>Sejarah</li>
         <li>Jurusan</li>
     </ul>
+
+
 </nav>
+
+<?php 
+
+if(isset($_POST['kirim'])){
+
+    $email = $_POST['email'];
+    $password= $_POST['password'];
+   
+    echo $email;
+    echo '<br>';
+    echo $password;
+   }
+
+
+?>
