ea795315f4fc0032a6b9971977f77d8e2a949e3a
FarahNurFadhilah <140325614+FarahNurFadhilah@users.noreply.github.com>
2024-02-15 12:50:48 +0700

    Paging

--- phpsmkrevit/restoran/kategori/select.php
+++ phpsmkrevit/restoran/kategori/select.php
@@ -1,8 +1,38 @@
+<div style="margin:auto; width:900px;">
 <?php 
 
     require_once "../function.php";
 
-    $sql = "SELECT * FROM tblkategori";
+    $sql = "SELECT idkategori FROM tblkategori";
+    $result = mysqli_query($koneksi,$sql);
+
+    $jumlahdata= mysqli_num_rows($result);
+
+    // echo $jumlahdata;
+
+    // $mulai =3;
+    $banyak=3;
+
+    $halaman =ceil($jumlahdata / $banyak);
+
+    for ($i=1; $i <=$halaman ; $i++) { 
+        echo '<a href="?p='.$i.'">'.$i.'</a>';
+        echo '&nbsp &nbsp &nbsp';
+    }
+
+    // echo $halaman;
+    echo '<br> <br>';
+
+    if (isset($_GET['p'])) {
+        $p=$_GET['p'];
+        // echo $p;
+        $mulai = ($p * $banyak) - $banyak;
+        //    = (5 * 3)-3;
+    }else{
+        $mulai = 0;
+    }
+
+    $sql = "SELECT * FROM tblkategori LIMIT $mulai,$banyak";
 
     $result = mysqli_query($koneksi, $sql);
 
@@ -20,7 +50,7 @@
     </tr>
     
     ';
-    $n0=1;
+    $n0=$mulai+1;
     if($jumlah > 0){
         while ($row = mysqli_fetch_assoc($result)){
             echo ' <tr>';
@@ -33,3 +63,4 @@
 
 ?>
 
+</div>
