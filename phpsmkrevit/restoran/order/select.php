cbb868b625dec650c47250943c082130857796ce
FarahNurFadhilah <140325614+FarahNurFadhilah@users.noreply.github.com>
2024-02-23 14:55:12 +0700

    Menampilkan Order Pembelian

--- phpsmkrevit/restoran/order/select.php
+++ phpsmkrevit/restoran/order/select.php
@@ -1,1 +1,73 @@
-<h1>Select Order</h1>
+<?php 
+   
+    $jumlahdata = $db->rowCount("SELECT idorder FROM vorder");
+    $banyak = 2;
+    $halaman = ceil($jumlahdata/$banyak);
+
+    if (isset($_GET['p'])) {
+        $p=$_GET['p'];
+        $mulai = ($p * $banyak) - $banyak;
+       
+    }else{
+        $mulai = 0;
+    }
+
+    $sql = "SELECT * FROM vorder  ORDER BY status ASC LIMIT $mulai,$banyak";
+    $row = $db->getAll($sql);
+
+    // var_dump($row);
+
+    $no=1+$mulai;
+
+?>
+
+
+
+<h3>Order PEMBELIAN</h3>
+<table class="table table-bordered w-50">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Pelanggan</th>
+            <th>Tanggal</th>
+            <th>Total</th>
+           <th>Bayar</th>
+           
+        </tr>
+    </thead>
+    <tbody>
+        <?php if(!empty($row)){?>
+        <?php foreach($row as $r): ?>
+            <?php 
+                
+                if ($r('status')== 0) {
+                    $status = '<td><a href="?f=order&m=bayar&id='.$r['idorder'].'">Bayar</a></td>';
+                }else{
+                   $status = '<td>Lunas</td>'; 
+                }
+                
+                ?>
+        <tr>
+            <td><?php echo $no++ ?></td>
+            <td><?php echo $r['pelanggan']?></td>
+            <td><?php echo $r['tglorder']?></td>
+            <td><?php echo $r['total']?></td>
+           <?php echo $status;?>
+           
+        </tr>
+        <?php endforeach ?>
+        <?php } ?>
+    </tbody>
+</table>
+
+<?php 
+
+for ($i=1; $i <=$halaman ; $i++) { 
+    echo '<a href="?f=order&m=select&p='.$i.'">'.$i.'</a>';
+    echo '&nbsp &nbsp &nbsp';
+}     
+
+
+?>
+
+
